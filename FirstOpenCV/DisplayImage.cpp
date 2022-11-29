24d6f581257dc58aa405b7df5e10deace8320618
Gustavo Jodar <gustavo.jodar@gmail.com>
2022-11-30 00:30:46 +0100

    add first type of ascii art with opencv

--- FirstOpenCV/DisplayImage.cpp
+++ FirstOpenCV/DisplayImage.cpp
@@ -1,22 +1,56 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 using namespace cv;
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], 1 );
+
+
+
+int main(){
+    //lendo a imagem
+    string path = "../images/lenna.png";
+    Mat image = imread(path);
+    Mat aux, img_gray;
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+
+    //convertendo a imagem para preto e branco
+    cvtColor(image, aux, COLOR_BGR2GRAY);
+    resize(aux, img_gray, Size(75, 75));
+
+    /*
+     "Standard" character ramp for grey scale pictures, black -> white.
+        "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. "
+    A more convincing but shorter sequence for representing 10 levels of grey is
+        " .:-=+*#%@"
+    */
+    //using the shorter:
+    string ASCII_gray_scale = " .:-=+*#%@";
+
+    //creating the string
+    string text = "";
+    for(int i = 0 ; i < img_gray.rows; i++){
+        for(int j = 0 ; j < img_gray.cols; j++){
+            //for the future -> 0 -> min(pixel_image) and 256 -> max(pixel_image)
+            int index  = ASCII_gray_scale.size()*((int)img_gray.at<uchar>(i,j) - 0)/256;
+            text.push_back(ASCII_gray_scale[index]);
+        }
+        text.push_back('\n');
+    }
+    cout << text;
+
+    //imshow("Teste", img_gray);
+    //waitKey(0);
+    
+    
     return 0;
 }
+
+
+
