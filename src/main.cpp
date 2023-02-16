bb8e18a634ae01a47dd31afa81fdff5063330ac5
tr0fin0 <guitrofino@gmail.com>
2023-02-16 15:04:52 +0100

    💩 edit main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
 #include "~Util/~util.h"
 #include <gtkmm/application.h>
 #include "GUI/screen.h"
+// #include "GUI/screen_1.h"
+// #include "networkConnection/tcp_s.cpp"
 
 int main(){
-    run();
+    // run();
     
     auto app = Gtk::Application::create("org.gtkmm.example");
 
-    ExampleWindow helloworld;
+    mainWindow Window;
 
-    //Shows the window and returns when it is closed.
-    return app->run(helloworld);
-    /*
+    // Shows the window and returns when it is closed.
+    return app->run(Window);
     
-    int choice = 0;
-    while(choice != 1 && choice != 2){    
-        std::cout << "(1)Server | (2) Client: ";
-        std::cin >> choice;
-    }
-    if(choice == 2){
-        be_client("147.250.224.185");
-    }
-    else{
-        system("hostname -I");
-        be_server();
-    }
-    */
-    return 0;
+    // int choice = 0;
+    // while(choice != 1 && choice != 2){    
+    //     std::cout << "(1)Server | (2) Client: ";
+    //     std::cin >> choice;
+    // }
+    // if(choice == 2){
+    //     be_client("147.250.224.185");
+    // }
+    // else{
+    //     system("hostname -I");
+    //     be_tcp_server();
+    // }
+    // return 0;
 }
