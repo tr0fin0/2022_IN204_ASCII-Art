db7aac2dd832ddf5c761761c1904bd08a528113c
tr0fin0 <guitrofino@gmail.com>
2023-02-16 15:05:15 +0100

    ✨ add dimensions file

---
+++ src/GUI/dimensions.h
@@ -0,0 +1,30 @@
+#include <string>
+
+int windowW = 1080;
+int windowH =  720;
+int windowB =   10;
+
+
+int boxImgX = 10;
+int boxImgY = 10;
+
+int boxReturnX = 10;
+int boxReturnY = 10;
+
+int boxButtonsX = 200;
+int boxButtonsY = 200;
+
+
+int alignReturnW = 200;
+int alignReturnH = 100;
+int alignReturnB =  10;
+
+int alignButtonsW = 400;
+int alignButtonsH = 400;
+int alignButtonsB =  10;
+
+
+int buttonReturnB  = 10;
+int buttonConvertB = 2;
+int buttonWebcamB  = 2;
+int buttonWebconB  = 2;
