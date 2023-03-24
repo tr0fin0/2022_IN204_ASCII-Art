489506b2266ee093ffd05758fd9ce142709bb1b1
tr0fin0 <guitrofino@gmail.com>
2023-03-24 22:56:43 +0100

    🚧 add basic dimensions

--- src/GUI/dimensions.h
+++ src/GUI/dimensions.h
@@ -1,30 +1,30 @@
 #include <string>
 
-int windowW = 1080;
-int windowH =  720;
+
+int windowW = 1000;
+int windowH =  380;
 int windowB =   10;
 
 
-int boxImgX = 10;
-int boxImgY = 10;
-
-int boxReturnX = 10;
-int boxReturnY = 10;
-
-int boxButtonsX = 200;
-int boxButtonsY = 200;
+int boxImgX = 0;
+int boxImgY = 0;
 
 
-int alignReturnW = 200;
-int alignReturnH = 100;
-int alignReturnB =  10;
+int alignReturnW = 0;       // 0
+int alignReturnH = 0;       // 0
+int alignReturnB = 0;       // 0
+int buttonReturnW  = 80;    // 10
+int buttonReturnH  = 10;    // 10
+int buttonReturnB  = 10;    // 10
+int boxReturnX = boxImgX;
+int boxReturnY = boxImgY;
 
-int alignButtonsW = 400;
-int alignButtonsH = 400;
-int alignButtonsB =  10;
 
-
-int buttonReturnB  = 10;
-int buttonConvertB = 2;
-int buttonWebcamB  = 2;
-int buttonWebconB  = 2;
+int alignButtonsW = 0;
+int alignButtonsH = 0;
+int alignButtonsB = 0;  // 0
+int buttonsW = 160;      // 1
+int buttonsH = 10;      // 1
+int buttonsB = 1;       // 1
+int boxButtonsX = windowW/2-buttonsW/2;
+int boxButtonsY = windowH;
