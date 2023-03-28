88c187188d9e93a8c70ec391df47c5d83a52941f
tr0fin0 <guitrofino@gmail.com>
2023-03-28 10:04:53 +0200

    ✨ add Webcam class file

---
+++ src/GUI/Webcam.h
@@ -0,0 +1,56 @@
+#include <gtkmm.h>
+#include <glibmm/dispatcher.h>
+#include <thread>
+#include <chrono>
+
+class Webcam : public Gtk::Window
+{
+public:
+    Webcam()
+    {
+        int windowW = 500;
+        int windowH = 500;
+
+        m_textview = Gtk::manage(new Gtk::TextView());
+        m_textview->set_editable(false);
+        m_textview->set_cursor_visible(false);
+        m_textview->set_size_request(windowW, windowH);
+
+        Gtk::Box *box = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL));
+        box->pack_start(*m_textview, false, false, 0);
+        box->set_size_request(windowW, windowH);
+
+        add(*box);
+
+        set_title("ASCII Webcam");
+        set_default_size(windowW, windowH);
+        set_border_width(10);
+        set_position(Gtk::WindowPosition::WIN_POS_CENTER_ALWAYS);
+        set_visible(true);
+        set_can_focus(false);
+        set_resizable(false);
+
+        m_dispatcher.connect([this]()
+                             {
+            Glib::RefPtr<Gtk::TextBuffer> buffer = m_textview->get_buffer();
+            buffer->set_text(m_ascii_text); });
+    }
+
+    void updateAsciiText(const std::string &ascii_text)
+    {
+        m_ascii_text = ascii_text;
+        m_dispatcher.emit();
+    }
+
+private:
+    void on_button_quit();
+
+    Gtk::TextView *m_textview;
+    std::string m_ascii_text;
+    Glib::Dispatcher m_dispatcher;
+};
+
+void Webcam::on_button_quit()
+{
+    close();
+}
