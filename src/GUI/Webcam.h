92ec5110bd39fe5acaefbbcaae2a202dcb1945e2
tr0fin0 <guitrofino@gmail.com>
2023-03-28 21:03:42 +0200

    💩 add Webcam printing

--- src/GUI/Webcam.h
+++ src/GUI/Webcam.h
@@ -2,6 +2,13 @@
 #include <glibmm/dispatcher.h>
 #include <thread>
 #include <chrono>
+#include <uchardet/uchardet.h>
+
+#include <glib.h>
+#include <iconv.h>
+#include <iostream>
+#include <string>
+#include <cstring>
 // #include "../converters/VideoConverter.h"
 // #include "../converters/ImageConverter.h"
 // #include "../converters/Converter.h"
@@ -11,13 +18,18 @@ class Webcam : public Gtk::Window
 public:
     Webcam()
     {
-        int windowW = 500;
-        int windowH = 500;
+        int windowW = 1000;
+        int windowH = 1000;
 
         m_textview = Gtk::manage(new Gtk::TextView());
         m_textview->set_editable(true);
         m_textview->set_cursor_visible(false);
         m_buffer = m_textview->get_buffer();
+
+        m_buffer->set_property("monospace", true);
+
+        m_textview->override_font(Pango::FontDescription("Monospace 12"));
+        m_textview->set_justification(Gtk::JUSTIFY_CENTER);
 
         add(*m_textview);
 
@@ -34,14 +46,88 @@ public:
 
         m_thread = std::thread([this]()
                                {
-            while (true)
-            {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                std::string new_text = get_new_text();
+                                   cv::VideoCapture cap(0);
+                                   cv::Mat img;
+                                   while (true)
+                                   {
+                                       std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                                       // std::string new_text = get_new_text();
+                                       Converter c;
+                                       cap.read(img);
 
-                m_ascii_text = new_text;
-                m_dispatcher.emit();
-            } });
+                                       // system("cls || clear");
+
+                                       c.setImage(img);
+                                       c.convertGrayScale();
+
+                                       c.resize(50, 50);
+                                       // c.print_ASCII(c.parallelConvert(c.getImage(), 1, 3));
+
+                                       // #include <glib.h>
+
+                                       // bool is_valid_utf8(const std::string& str) {
+                                       //     return g_utf8_validate(str.c_str(), str.length(), NULL);
+                                       // }
+
+                                       // return "constant\nconstant\n\nconstant";
+                                       std::string str = c.getStringASCII();
+
+                                       // std::cout << g_utf8_validate(str.c_str(), str.length(), NULL) << std::endl;
+
+                                       // // const char* invalidString = "This is an invalid string\x80";
+                                       // std::string validString;
+                                       // validString.assign(str, strlen(str.c_str()));
+                                       // std::cout << "Invalid string:\n"
+                                       //           << str << std::endl;
+                                       // std::cout << "Valid string:\n"
+                                       //           << validString << std::endl;
+
+                                       uchardet_t ud = uchardet_new();
+                                       uchardet_handle_data(ud, str.c_str(), str.size());
+                                       uchardet_data_end(ud);
+                                       const char *encoding = uchardet_get_charset(ud);
+                                       std::string result(encoding);
+                                       // std::cout << "encoding: " << encoding << std::endl;
+                                       uchardet_delete(ud);
+
+                                       // Create an iconv descriptor for the conversion
+                                       iconv_t cd = iconv_open("UTF-8", "ASCII");
+
+                                       std::string ascii_str = str;
+
+                                       // Get the length of the input and output strings
+                                       size_t in_len = ascii_str.length();
+                                       size_t out_len = in_len * 4; // Maximum possible size for UTF-8
+
+                                       // Allocate memory for the output string
+                                       char *out_buf = new char[out_len + 1];
+
+                                       // Convert the string
+                                       char *in_ptr = const_cast<char *>(ascii_str.c_str());
+                                       char *out_ptr = out_buf;
+                                       if (iconv(cd, &in_ptr, &in_len, &out_ptr, &out_len) == (size_t)-1)
+                                       {
+                                           perror("iconv");
+                                           exit(EXIT_FAILURE);
+                                       }
+
+                                       // Null-terminate the output string
+                                       *out_ptr = '\0';
+
+                                       // Clean up
+                                       iconv_close(cd);
+                                       std::string utf8_str(out_buf);
+                                       delete[] out_buf;
+                                       // std::cout << "utf8_str:\n" << utf8_str << std::endl;
+                                       // return str;
+                                       // return utf8_str;
+
+                                       // m_ascii_text = new_text;
+                                       m_ascii_text = utf8_str;
+                                       m_dispatcher.emit();
+                                   }
+                                   // cap.release(); // TODO kill camera input after closing window
+                               });
         show_all_children();
     }
 
@@ -60,7 +146,7 @@ private:
     Glib::Dispatcher m_dispatcher;
     std::thread m_thread;
 
-    std::string get_new_text();
+    // std::string get_new_text();
     void on_button_quit();
 };
 
@@ -69,72 +155,9 @@ void Webcam::on_button_quit()
     close();
 }
 
-std::string Webcam::get_new_text()
-{
-    // Get new text from somewhere, e.g. a file, a network connection, etc.
-    // For this example, just return a static string.
-    // cv::VideoCapture cap(0);
-    // cv::Mat img;
-    // Converter c;
-    // cap.read(img);
-
-    // system("cls || clear");
-
-    // c.setImage(img);
-    // c.convertGrayScale();
-
-    // c.resize(65, 50);
-
-    // c.print_ASCII(c.parallelConvert(c.getImage(), 1, 3));
-
-
-    // return c.getStringASCII();
-    return "constant";
-}
-
-// class Webcam : public Gtk::Window
+// std::string Webcam::get_new_text()
 // {
-// public:
-//     Webcam()
-//     {
-//         int windowW = 500;
-//         int windowH = 500;
-
-//         m_textview = Gtk::manage(new Gtk::TextView());
-//         m_textview->set_editable(false);
-//         m_textview->set_cursor_visible(false);
-//         m_textview->set_size_request(windowW, windowH);
-
-//         Gtk::Box *box = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL));
-//         box->pack_start(*m_textview, false, false, 0);
-//         box->set_size_request(windowW, windowH);
-
-//         add(*box);
-
-//         set_title("ASCII Webcam");
-//         set_default_size(windowW, windowH);
-//         set_border_width(10);
-//         set_position(Gtk::WindowPosition::WIN_POS_CENTER_ALWAYS);
-//         set_visible(true);
-//         set_can_focus(false);
-//         set_resizable(false);
-
-//         m_dispatcher.connect([this]()
-//                              {
-//             Glib::RefPtr<Gtk::TextBuffer> buffer = m_textview->get_buffer();
-//             buffer->set_text(m_ascii_text); });
-//     }
-
-//     void updateAsciiText(const std::string &ascii_text)
-//     {
-//         m_ascii_text = ascii_text;
-//         m_dispatcher.emit();
-//     }
-
-// private:
-//     void on_button_quit();
-
-//     Gtk::TextView *m_textview;
-//     std::string m_ascii_text;
-//     Glib::Dispatcher m_dispatcher;
-// };
+//     // Get new text from somewhere, e.g. a file, a network connection, etc.
+//     // For this example, just return a static string.
+//     return "true";
+// }
