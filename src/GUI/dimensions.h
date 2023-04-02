461aa552a1470c81a6a27bc07b092683fb43e8e9
tr0fin0 <guitrofino@gmail.com>
2023-04-02 19:06:41 +0200

    ✨ add boxConference dimensions

--- src/GUI/dimensions.h
+++ src/GUI/dimensions.h
@@ -14,21 +14,27 @@ int boxImgX = (windowW - imageW)/2;
 int boxImgY = (windowH - imageH)/2;
 
 
-int alignReturnW = 0;       // 0
-int alignReturnH = 0;       // 0
-int alignReturnB = 0;       // 0
 int buttonIPW  = 80;    // 10
 int buttonIPH  = 10;    // 10
 int buttonIPB  = 10;    // 10
+int alignReturnW = 0;       // 0
+int alignReturnH = 0;       // 0
+int alignReturnB = 0;       // 0
 int boxReturnX = boxImgX;
 int boxReturnY = boxImgY;
 
 
+int buttonsW = 240;      // 1
+int buttonsH = 10;      // 1
+int buttonsB = 1;       // 1
 int alignButtonsW = 0;
 int alignButtonsH = 0;
 int alignButtonsB = 0;  // 0
-int buttonsW = 160;      // 1
-int buttonsH = 10;      // 1
-int buttonsB = 1;       // 1
 int boxButtonsX = windowW/2-buttonsW/2;
 int boxButtonsY = windowH;
+
+
+int entryW = 200;      // 1
+int buttonConnectW = buttonsW - entryW;      // 1
+int boxConferenceX = windowW/2-buttonsW/2;
+int boxConferenceY = 100;
